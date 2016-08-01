b27f729d396f597218e1d8da8ea31ea4044330e0
tmullen <tony.mullen@gmail.com>
2016-08-01 14:21:32 -0700

    add separate controller file

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-var homepageController = function(req, res, next) {
-  res.render('index', { title: 'Express' });
-};
+router.get('/', ctrlMain.index);
 
 module.exports = router;
-
-/*GET home page. */
-router.get('/', homepageController);
