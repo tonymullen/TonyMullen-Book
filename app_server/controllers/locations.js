dfaebb7fdb8bb0c9c50821966275b820b9a26744
tmullen <tony.mullen@gmail.com>
2016-08-01 22:44:34 -0700

    loop through locations

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,26 @@ module.exports.homelist = function(req, res) {
     pageHeader: {
       title: 'Loc8r',
       strapline: 'Find places to work with wifi near you!'
+    }, locations: [{
+      name: 'Oppenheimer Cafe',
+      address: '1500 N Warner St, Tacoma, WA 98416',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '200m'
+    }, {
+      name: 'Cafe Hero',
+      address: '11223 High Street, Tacoma, WA, 98407',
+      rating: 4,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '200m'
+    }, {
+      name: 'Cafe de Couture',
+      address: '11224 Lowe Street, Tacoma, WA, 98407',
+      rating: 2,
+      facilities: ['Hot drinks', 'Premium wifi'],
+      distance: '240m'
     }
+  ]
   });
 };
 
