a53efcfe3b432417182be6f4faf8fa1c3a920e09
tmullen <tony.mullen@gmail.com>
2016-08-02 00:18:07 -0700

    process termination events

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -11,4 +11,29 @@ mongoose.connection.on('error', function(err) {
 });
 mongoose.connection.on('disconnected', function() {
   console.log('Mongoose disconnected');
-})
+});
+
+var gracefulShutdown = function(msg, callback) {
+  mongoose.connection.close(function() {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  })
+};
+
+process.once('SIGUSR2', function() {
+  gracefulShutdown('nodemon restart', function() {
+    process.kill(process.pid, 'SIGSR2');
+  });
+});
+
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function() {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function() {
+    process.exit(0);
+  });
+});
