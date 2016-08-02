b600c12f6ac6a934c0d3b23e0a2391131f7f6eec
tmullen <tony.mullen@gmail.com>
2016-08-02 00:09:26 -0700

    local db and mongoose connection

---
+++ app_server/models/db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+})
