0b55ff5a53464ae76a6b5bfc289c6a9951e377f2
tmullen <tony.mullen@gmail.com>
2016-08-02 22:05:29 -0700

    add create location

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -35,9 +35,38 @@ module.exports.locationsListByDistance = function (req, res) {
     sendJsonResponse(res, 200, locations);
   });
 };
+
 module.exports.locationsCreate = function (req, res) {
-  sendJsonResponse(res, 200, {"status": "success"});
+  Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed:  req.body.closed1
+    },{
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed:  req.body.closed2
+    },{
+      days: req.body.days3,
+      opening: req.body.opening3,
+      closing: req.body.closing3,
+      closed:  req.body.closed3
+    }]
+  }, function(err, location) {
+    if (err) {
+      sendJsonResponse(res, 400, err);
+    } else {
+      sendJsonResponse(res, 201, location);
+    }
+  })
 };
+
 module.exports.locationsReadOne = function (req, res) {
   console.log("Reading one");
   if (req.params && req.params.locationid) {
