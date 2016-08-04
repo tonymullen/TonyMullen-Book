0d5fc2e38fbd455d985349c7be8a63440e45a4d6
tmullen <tony.mullen@gmail.com>
2016-08-04 00:43:05 -0700

    add directives and filters for homepage

---
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -0,0 +1,14 @@
+angular
+  .module('loc8rApp')
+  .directive('ratingStars', ratingStars);
+
+
+function ratingStars() {
+  return {
+    restrict: 'EA',
+    scope: {
+      thisRating: '=rating'
+    },
+    templateUrl: '/common/directives/ratingStars/rating-stars.html'
+  };
+};
