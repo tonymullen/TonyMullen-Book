fb9a5fc6210a7786ae2480a3119d9606f817cbbb
Tony Mullen <tony.mullen@gmail.com>
2017-12-11 18:54:18 -0800

    adds bootstrap & deps

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
