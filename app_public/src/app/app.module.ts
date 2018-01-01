11c9b45c35edcc39199087d644767bb00eeda7ca
Tony Mullen <tony.mullen@gmail.com>
2017-12-31 23:33:16 -0800

    adds homelist-component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { HomeListComponent } from './home-list/home-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeListComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeListComponent]
 })
 export class AppModule { }
