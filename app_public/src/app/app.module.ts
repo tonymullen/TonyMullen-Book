0ce7d071dd3ad924c78f74c574f3614073469159
Tony Mullen <tony.mullen@gmail.com>
2018-04-17 21:54:58 -0700

    adds routes and about component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { HomeListComponent } from './home-list/home-list.component';
 import { DistancePipe } from './distance.pipe';
-
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeListComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
