26b7830b51aa513c4c8cadffde904fbcb24dfd3c
Tony Mullen <tony.mullen@gmail.com>
2018-05-23 18:17:05 -0700

    sets up user object and jwt

---
+++ app_api/models/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pdkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  const hash = crypto.pdkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime()/ 1000),
+  }, 'thisIsSecret');
+};
