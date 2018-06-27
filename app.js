03e30cbc5119c59ecf30739bcea97336baab4b46
Tony Mullen <tony.mullen@gmail.com>
2018-06-27 13:21:27 -0700

    add comment in app.js

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ app.use(function (err, req, res, next) {
 
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  // res.locals are only available to the view rendered on
+  // this request/response cycle
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
